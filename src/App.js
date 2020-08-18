8fd55d804a18c9561459ff01948641f220d3e313
slawoe <info@slawomirernst.de>
2020-08-18 15:44:34 +0200

    Add useEffect and map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import fetchToDos from "./api/fetchToDos";
 
 function App() {
-  fetchToDos();
-  return <div>Hallo</div>;
+  const [toDos, setToDos] = useState(null);
+  useEffect(() => {
+    async function doFetch() {
+      const fetchData = await fetchToDos();
+      setToDos(fetchData);
+    }
+    doFetch();
+  }, []);
+
+  return (
+    <div>
+      {toDos?.map((todo) => (
+        <h3 key={todo.id}>{todo.task}</h3>
+      ))}
+    </div>
+  );
 }
 
 export default App;
